31a8c38e442e20cbd2d4a55b2e9ff1dfa7174b06
thibaultmourlane7 <thibaultmourlane7@gmail.com>
2026-09-23 09:41:17 +0200

    V19: publish app bundle with Ideas

--- src/auth.js
+++ src/auth.js
@@ -394,7 +394,7 @@
 
     return new Promise((resolve, reject) => {
       const script = document.createElement('script');
-      script.src = './src/app.js?v=20260923-1';
+      script.src = './src/app.js?v=20260923-2';
       script.type = 'module';
       script.dataset.pilotageApp = 'true';
 
